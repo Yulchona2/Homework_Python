4b2a904ca394065c97d3963f19eda4c08b9f5988
Youlchona2 <yulchona2@gmail.com>
2025-07-19 23:36:49 +0300

    changed url in lesson05_task2.py

--- lesson_05/lesson05_task2.py
+++ lesson_05/lesson05_task2.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
-driver.get("http://uitestingplayground.com/classattr")
+driver.get("http://uitestingplayground.com/dynamicid")
 driver.find_element(By.CSS_SELECTOR, "button[class*='btn-primary']").click()
 
 driver.quit()
